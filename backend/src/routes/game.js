e5a39838a54fa88eaa3f2aed9d8bf4fff46aa220
michael1nguyen1 <michael22nguyen@gmail.com>
2025-04-13 20:15:24 +0300

    added fastify initilization in game.js

--- backend/src/routes/game.js
+++ backend/src/routes/game.js
@@ -1,19 +1,24 @@
+const { authenticate } = require('../middleware/auth');
 const { validateFriendship } = require('../middleware/friendValidation');
 
-fastify.post('/invite/:friendId', { preHandler: [authenticate, validateFriendship] }, async (request, reply) => {
-  const userId = request.user.id;
-  const friendId = parseInt(request.params.friendId);
+async function gameRoutes(fastify, options) {
+  fastify.post('/invite/:friendId', { preHandler: [authenticate, validateFriendship] }, async (request, reply) => {
+    const userId = request.user.id;
+    const friendId = parseInt(request.params.friendId);
 
-  // Emit game invitation
-  const friendSocket = fastify.io.sockets.get(friendId);
-  if (friendSocket) {
-    friendSocket.emit('game_invitation', {
-      from: userId,
-      username: request.user.username,
-    });
-    return { success: true, message: 'Game invitation sent' };
-  }
+    // Emit game invitation
+    const friendSocket = fastify.io.sockets.get(friendId);
+    if (friendSocket) {
+      friendSocket.emit('game_invitation', {
+        from: userId,
+        username: request.user.username,
+      });
+      return { success: true, message: 'Game invitation sent' };
+    }
 
-  reply.code(404);
-  return { error: 'User is not online' };
-});
+    reply.code(404);
+    return { error: 'User is not online' };
+  });
+}
+
+module.exports = gameRoutes;
