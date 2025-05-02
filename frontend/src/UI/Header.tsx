e90564c00b494c1abcf054cccb299e3d92032bd0
fjjdessoycaraballo <fdessoycaraballo@gmail.com>
2025-05-02 16:21:52 +0300

    feature: adding assets

--- frontend/src/UI/Header.tsx
+++ frontend/src/UI/Header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
 
 export const Header: React.FC<HeaderProps> = ({ onClick }) => {
 	return (
-		<header className="fixed top-0 left-0 w-full bg-grey z-50 shadow-md">
+		<header className="fixed top-0 left-0 w-full bg-[url('../assets/header.png')] z-[9999] shadow-md">
 			<div className="container mx-auto px-4 py-3 flex justify between items-center">
 				<h1 className="titles ">Transcendence</h1>
 				{/*placeholder for anything else that could go with title*/}
