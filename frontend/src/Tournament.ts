9ee89d012fd97b8f70a40c092fa49f1bc5cb509e
Panu Kangas <kangas.panu@gmail.com>
2025-05-03 23:15:51 +0300

    Tournament logic is finished, but not polished. This will be done later. Also small improvements were done to UserHUB, for example the players are now displayed based on ranking difference, so it's easier to find well balanced matches between other players.

--- frontend/src/Tournament.ts
+++ frontend/src/Tournament.ts
@@ -1,16 +1,58 @@
-import { ctx } from "./index"; // Sort of weird to use this globally here to pass it to InGame...
+import { ctx, stateManager } from "./index"; // Sort of weird to use this globally here to pass it to InGame...
 import { GameStates, IGameState, } from "./GameStates";
 import { TEXT_PADDING } from "./constants";
 import { User } from "./UserManager";
 import { ReturnMainMenuButton } from "./EndScreen";
+import { InGame } from "./InGame";
+import { Button } from "./Button";
+import { MatchIntro } from "./MatchIntro";
+import { MainMenu } from "./MainMenu";
+import { EndScreen } from "./EndScreen";
+
+export interface TournamentPlayer
+{
+	id: number;
+	user: User;
+	tournamentPoints: number;
+	place: number;
+	coinsCollected: number;
+	isWinner: boolean;
+}
+
+const GameOrder: [number, number][] = [
+	[0, 1],
+	[2, 3],
+	[0, 2],
+	[1, 3],
+	[3, 0],
+	[2, 1]
+];
+
+
+export class NextGameBtn extends Button
+{
+	constructor(x: number, y: number, boxColor: string, hoverColor: string, text: string, textColor: string, textSize: string, font: string)
+	{
+		super(x, y, boxColor, hoverColor, text, textColor, textSize, font);
+	}
+
+	clickAction(): void 
+	{
+	}
+}
 
 
 export class Tournament implements IGameState
 {
 	name: GameStates;
 	canvas: HTMLCanvasElement;
-	playerArr: User [];
+	playerArr: TournamentPlayer [];
+	curMatch: MatchIntro | InGame | EndScreen | null;
+	matchCounter: number;
+	isFinished: boolean;
+	tournamentWinner: TournamentPlayer [];
 	returnMenuButton: ReturnMainMenuButton;
+	nextGameBtn: NextGameBtn;
 	mouseMoveBound: (event: MouseEvent) => void;
     mouseClickBound: () => void;
 
@@ -18,14 +60,38 @@ export class Tournament implements IGameState
 	{
 		this.name = GameStates.TOURNAMENT;
 		this.canvas = canvas;
-		this.playerArr = players;
+		this.playerArr = [];
+		this.curMatch = null;
+		this.matchCounter = 0;
+		this.isFinished = false;
+		this.tournamentWinner = [];
 
-		let text1 = 'RETURN TO MENU';
+		// Create TournamentPlayer array
+		for (let i = 0; i < 4; i++)
+		{
+			let tournamentObj: TournamentPlayer = {
+				id: i,
+				user: players[i],
+				place: 0,
+				tournamentPoints: 0,
+				coinsCollected: 0,
+				isWinner: false
+			};
+
+			this.playerArr.push(tournamentObj);
+		}
+
+		let text1 = 'EXIT TOURNAMENT'; // Add a warning here that all tournament data will be lost
 		ctx.font = '25px arial' // GLOBAL USE OF CTX!!
-		const button1X = (canvas.width / 2) - (ctx.measureText(text1).width / 2) - TEXT_PADDING;
-		const button1Y = (canvas.height / 2) - 20 - TEXT_PADDING + 370;
-
+		const button1X = 20;
+		const button1Y = 20;
 		this.returnMenuButton = new ReturnMainMenuButton(button1X, button1Y, 'red', '#780202', text1, 'white', '25px', 'arial');
+
+		let text2 = 'NEXT GAME';
+		ctx.font = '35px arial' // GLOBAL USE OF CTX!!
+		const button2X = (canvas.width / 2) - (ctx.measureText(text2).width / 2) - TEXT_PADDING;
+		const button2Y = (canvas.height / 2) - 20 - TEXT_PADDING + 270;
+		this.nextGameBtn = new NextGameBtn(button2X, button2Y, 'green', '#054d19', text2, 'white', '35px', 'arial');
 
 		this.mouseMoveBound = (event: MouseEvent) => this.mouseMoveCallback(event);
         this.mouseClickBound = () => this.mouseClickCallback();
@@ -45,11 +111,27 @@ export class Tournament implements IGameState
 
 		this.returnMenuButton.checkMouse(x, y);
 
+		if (!this.isFinished)
+			this.nextGameBtn.checkMouse(x, y);
 	}
 
 	mouseClickCallback()
 	{
 		this.returnMenuButton.checkClick();
+		
+		if (!this.isFinished && this.nextGameBtn.checkClick())
+		{
+			const id1 = GameOrder[this.matchCounter][0];
+			const id2 = GameOrder[this.matchCounter][1];
+			const player1 = this.playerArr.find(player => player.id === id1);
+			const player2 = this.playerArr.find(player => player.id === id2);
+
+			if (player1 && player2)
+			{
+				this.curMatch = new MatchIntro(this.canvas, player1.user, player2.user, player1, player2);
+				this.curMatch.enter();
+			}
+		}
 	}
 
 	enter()
@@ -66,53 +148,209 @@ export class Tournament implements IGameState
 
 	update(deltaTime: number)
 	{
+		if (this.curMatch)
+		{
+			this.curMatch.update(deltaTime);
+			if (this.curMatch.isStateReady)
+			{
+				const id1 = GameOrder[this.matchCounter][0];
+				const id2 = GameOrder[this.matchCounter][1];
+				const player1 = this.playerArr.find(player => player.id === id1);
+				const player2 = this.playerArr.find(player => player.id === id2);
+				this.curMatch.exit();
+				
+				if (this.curMatch.name === GameStates.MATCH_INTRO && player1 && player2)
+				{	
+					this.curMatch = new InGame(this.canvas, player1.user, player2.user, player1, player2);
+					this.curMatch.enter();
+				}
+				else if (this.curMatch.name === GameStates.IN_GAME && player1 && player2)
+				{
+					const winner = player1.isWinner ? player1 : player2;
+					const loser = player1.isWinner ? player2 : player1;
+
+					this.curMatch = new EndScreen(this.canvas, winner.user, loser.user, winner, loser);
+					this.curMatch.enter();
+					player1.isWinner = false;
+					player2.isWinner = false;
+
+					this.playerArr.sort((a, b) => {
+						if (b.tournamentPoints !== a.tournamentPoints)
+							return b.tournamentPoints - a.tournamentPoints;
+						return b.coinsCollected - a.coinsCollected;
+					})
+				}
+				else
+				{
+					this.curMatch = null;
+					this.matchCounter++;
+					if (this.matchCounter === 6)
+					{
+						this.isFinished = true;
+						this.findWinner();
+					}
+				}
+			}
+		}
+	}
+
+	findWinner(): void
+	{
+		this.playerArr.sort((a, b) => {
+			if (b.tournamentPoints !== a.tournamentPoints)
+				return b.tournamentPoints - a.tournamentPoints;
+			return b.coinsCollected - a.coinsCollected;
+		})
+
+		this.tournamentWinner.push(this.playerArr[0]);
+
+		if (this.playerArr[0].tournamentPoints === this.playerArr[1].tournamentPoints
+			&& this.playerArr[0].coinsCollected === this.playerArr[1].coinsCollected)
+		{
+			for (let i = 1; i < 4; i++)
+			{
+				if (this.playerArr[i].tournamentPoints === this.playerArr[0].tournamentPoints
+					&& this.playerArr[i].coinsCollected === this.playerArr[0].coinsCollected)
+					this.tournamentWinner.push(this.playerArr[i]);
+			}
+		}
 	}
 
 	drawScoreBoard(ctx: CanvasRenderingContext2D)
 	{
 		const colW = 200;
-		const colH = 100;
-		let x = this.canvas.width / 2 - colW;
-		let y = 200;
-		const padding = 50;
+		const colH = 80;
+		let x = this.canvas.width / 2 - colW * 1.5;
+		let y = 170;
+		const padding = 45;
 
-		for (const player of this.playerArr)
+		// Draw background
+		ctx.fillStyle = '#5e7291';
+		ctx.fillRect(x, y, 3 * colW, 5 * colH);
+
+		ctx.fillStyle = '#2b3442';
+		ctx.fillRect(x, y, 3 * colW, colH);
+		ctx.fillRect(x, y, colW, 5 * colH);
+
+		// Draw first row (names of columns)
+		ctx.strokeStyle = '#0a42ab';
+		ctx.lineWidth = 4;
+		ctx.strokeRect(x, y, colW, colH);
+
+		ctx.font = '40px Impact';
+		ctx.fillStyle = 'white';
+		const usernameText = 'USERNAME';
+		const usernameX = x + (colW / 2) - (ctx.measureText(usernameText).width / 2);
+		ctx.fillText(usernameText, usernameX, y + padding + 5);
+
+		ctx.strokeRect(x + colW, y, colW, colH);
+		const pointsText = 'POINTS';
+		const pointsX = x + colW + (colW / 2) - (ctx.measureText(pointsText).width / 2);
+		ctx.fillText(pointsText, pointsX, y + padding + 5);
+
+		ctx.strokeRect(x + colW * 2, y, colW, colH);
+		const coinText = 'COINS';
+		const coinX = x + (colW * 2) + (colW / 2) - (ctx.measureText(coinText).width / 2);
+		ctx.fillText(coinText, coinX, y + padding + 5);
+
+		y += colH;
+
+		// Draw player info
+		for (let i = 0; i < 4; i++)
 		{
+			const player = this.playerArr[i];
+			player.place = i + 1;
+
 			ctx.strokeStyle = '#0a42ab';
 			ctx.lineWidth = 4;
 			ctx.strokeRect(x, y, colW, colH);
 
 			ctx.font = '30px arial';
-			ctx.fillStyle = 'green';
-			const text = player.username;
+			ctx.fillStyle = 'white';
+			const text = player.place + '. ' + player.user.username;
 			const playerX = x + (colW / 2) - (ctx.measureText(text).width / 2);
-			const playerY = y + padding;
-			ctx.fillText(text, playerX, playerY);
+			ctx.fillText(text, playerX, y + padding);
 
 			ctx.strokeRect(x + colW, y, colW, colH);
 			ctx.fillStyle = 'white';
-			const scoreText = '0';
+			const scoreText = player.tournamentPoints.toString();
 			const scoreX = x + colW + (colW / 2) - (ctx.measureText(scoreText).width / 2);
-			const scoreY = y + padding;
-			ctx.fillText(scoreText, scoreX, scoreY);
+			ctx.fillText(scoreText, scoreX, y + padding);
+
+			ctx.strokeRect(x + colW * 2, y, colW, colH);
+			const coinText = player.coinsCollected.toString();
+			const coinX = x + colW * 2 + (colW / 2) - (ctx.measureText(coinText).width / 2);
+			ctx.fillText(coinText, coinX, y + padding);
 
 			y += colH;
 		}
-
-
 	}
 
 	render(ctx: CanvasRenderingContext2D)
 	{
-		const headerText = "SCORE BOARD";
-		ctx.font = '50px arial';
-		ctx.fillStyle = 'white';
-		const headerX = (this.canvas.width / 2) - (ctx.measureText(headerText).width / 2);
-		ctx.fillText(headerText, headerX, 140);
 
-		this.drawScoreBoard(ctx);
+		if (!this.curMatch)
+		{
+			// Draw header
+			const headerText = "SCORE BOARD";
+			ctx.font = '50px Impact';
+			ctx.fillStyle = 'white';
+			const headerX = (this.canvas.width / 2) - (ctx.measureText(headerText).width / 2);
+			ctx.fillText(headerText, headerX, 100);
+			
+			this.drawScoreBoard(ctx);
+	
+			// Draw Return button
+			this.returnMenuButton.draw(ctx);
+			let exitWarning;
+			if (!this.isFinished)
+				exitWarning = `(Exit now == lose tournament progress)`;
+			else
+				exitWarning = '(You can now safely exit the tournament)';
+			ctx.font = '20px arial';
+			ctx.fillStyle = 'white';
+			const warningX = this.returnMenuButton.x;
+			const warningY = this.returnMenuButton.y + this.returnMenuButton.height + 20;
+			ctx.fillText(exitWarning, warningX, warningY);
 
-		this.returnMenuButton.draw(ctx);
+			// Draw Next game button || Winner announcement
+			if (!this.isFinished)
+			{
+				this.nextGameBtn.draw(ctx);
+				const nextGameNum = `Game no. ${(this.matchCounter + 1).toString()}`;
+				ctx.font = '25px arial';
+				ctx.fillStyle = 'white';
+				const gameNumX = this.nextGameBtn.x;
+				const gameNumY = this.nextGameBtn.y + this.nextGameBtn.height + 20;
+				ctx.fillText(nextGameNum, gameNumX, gameNumY);
+			}
+			else
+			{
+				const headerText = "TOURNAMENT WINNER(s):";
+				ctx.font = '50px Impact';
+				ctx.fillStyle = 'green';
+				const headerX = (this.canvas.width / 2) - (ctx.measureText(headerText).width / 2);
+				let y = (this.canvas.height / 2) - 20 - TEXT_PADDING + 270;
+				ctx.fillText(headerText, headerX, y);
+				y += 40
+
+				for (const player of this.tournamentWinner)
+				{
+					const playerText = player.user.username;
+					ctx.font = '30px arial';
+					ctx.fillStyle = 'white';
+					const playerX = (this.canvas.width / 2) - (ctx.measureText(playerText).width / 2);
+					ctx.fillText(playerText, playerX, y);
+
+					y += 40;
+				}
+			}
+		}
+		else
+		{
+			this.curMatch.render(ctx);
+		}
+
 	}
 
 }
