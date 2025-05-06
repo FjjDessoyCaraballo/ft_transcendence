0b067c9724e29bcd43b5f4059681922578295048
fjjdessoycaraballo <fdessoycaraballo@gmail.com>
2025-05-06 10:58:43 +0300

    feature: TOS updated

--- frontend/src/UI/Registration.tsx
+++ frontend/src/UI/Registration.tsx
@@ -106,21 +106,37 @@ export const GDPRPopup: React.FC<WindowManager> = ({ onAccept, onDecline }) => {
 		return (
 			<div className="fixed inset-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-50">
 			  <div className="bg-white rounded-lg shadow-lg w-[600px] max-w-[90%] max-h-[80vh] flex flex-col overflow-hidden mx-auto">
-			  <h2 className="titles">Privacy notice</h2>
-				<div className="px-12 overflow-y-auto max-h-[500px] flex-grow">
+			  <h2 className="titles">Terms of Service and Privacy Notice</h2>
+				<div className="px-8 overflow-y-auto max-h-[500px] flex-grow">
 				<p className="texts">
-					This website stores data locally in your computer using 
-					localStorage to enhance your gaming experience.
-					This includes user account information and game metrics.
-					This information is saved in our servers for one day solely for the purpose
-					of displaying the gaming metrics for the player and making it possible for you
-					to log into your account again after the browser is closed.
-				  </p>
-				  <p></p>
+					Dads and Coders Inc. ("controller" and "processor") collects data for (mandatory) statistical purposes.
+					The data is collected during matches and processed in our servers. The data will be stored for a day solely
+					for the purposes of giving statistical insights to players in our two games: Pong and Block Battle.</p>
+				  <p className="texts">By pressing "Accept" button you are agreeing to the following:</p>
+				  <p className="texts">- Data collection by Dads and Coders Inc.</p>
+				  <p className="texts">- Data processing by Dads and Coders Inc.</p>
+				  <p className="texts">The data that Dads and Coders Inc. collect for the purposes processing are the following:</p>
+				  <p className="texts">- Id;</p>
+				  <p className="texts">- Username;</p>
+				  <p className="texts">- Password;</p>
+				  <p className="texts">- Ranking points;</p>
+				  <p className="texts">- Avatar;</p>
+				  <p className="texts">- Games played (Pong);</p>
+				  <p className="texts">- Wins (Pong);</p>
+				  <p className="texts">- Losses (Pong);</p>
+				  <p className="texts">- Games played (Block Battle);</p>
+				  <p className="texts">- Wins (Block Battle);</p>
+				  <p className="texts">- Losses (Block Battle);</p>
+				  <p className="texts">- Tournaments played;</p>
+				  <p className="texts">- Tournaments won;</p>
+				  <p className="texts">- Tournament points overall;</p>
+				  <p className="texts">- Friends;</p>
+				  <p className="texts">- MATCH HISTORY (an array of MATCHES). </p>
+				  <p className="texts"> REQUESTS FOR DATA DELETION: requests for data deletion can be made directly through the settings by end-user.</p>
+				  <p className="texts"> REQUESTS FOR OWN DATA: End-users that want to download all their data that is contained within Dads and Coders Inc. 
+					servers, it is possible to download the data directly from the settings section by pressing the "Download data" button.</p>
 				  <p className="texts">
-					For any inquiries or questions, you can contact the data protection officer: 
-					Felipe Dessoy, fdessoy-@hive.student.fi
-				  </p>
+				  If said end-user finds difficulties in deleting their data, they may contact data protection officer. For any inquiries or questions, you can contact the data protection officer: Felipe Dessoy, fdessoy-@hive.student.fi</p>
 				</div>
 				<div className="flex justify-end p-5 border-t border-gray-100 gap-2">
 					<button 
