e04a580b1fd71157ed970339041569556f620225
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-05-19 17:56:29 +0300

    feature: added download data functionality

--- frontend/src/UI/Settings.tsx
+++ frontend/src/UI/Settings.tsx
@@ -67,7 +67,17 @@ export const SettingsPopup: React.FC<SettingsProps> = ({ onClick }) => {
 
 	const HandleDownloadData = async () => {
 		try {
-			await getUserDataForDownload();
+			const userData = await getUserDataForDownload();
+			const jsonString = JSON.stringify(userData);
+			const blob = new Blob([jsonString], {type: '/application/json'});
+			const url = URL.createObjectURL(blob);
+			const a = document.createElement('a');
+			a.href = url;
+			a.download = `user_data_${Date.now()}.json`;
+			document.body.appendChild(a);
+			a.click();
+			document.body.removeChild(a);
+			URL.revokeObjectURL(url);
 		} catch (error) {
 			throw new Error('Could not fetch data at the moment. Try again later or contact data protection officer.')
 		}
