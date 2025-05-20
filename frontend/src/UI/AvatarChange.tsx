61611ea5a60d9f80e7a941ae54dd93537d853974
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-05-20 13:39:19 +0300

    feature: added box for uploaded avatar

--- frontend/src/UI/AvatarChange.tsx
+++ frontend/src/UI/AvatarChange.tsx
@@ -27,7 +27,8 @@ export const AvatarChangePopup: React.FC<{onClick: () => void}> = ({ onClick }) 
 			<form onSubmit={HandleNewAvatar}
 			className="p-6">
 				<label htmlFor="avatar" className="buttons">Avatar</label>
-				<input 
+				<input
+				className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#800080]"
 				type="file"
 				id="avatar" 
 				name="avatar"
