2808b80d1df56b71806f78a5763733ade08b0325
michael1nguyen1 <michael22nguyen@gmail.com>
2025-05-29 14:06:17 +0300

    added health check

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,6 +3,10 @@ async function routes(fastify, options) {
 	fastify.register(require('./game'), { prefix: '/api/games' });
 	fastify.register(require('./friend'), { prefix: '/api/friends' });
 	
+	fastify.get('/api/health', async (request, reply) => {
+		return { status: 'ok', message: 'API is running' };
+	});
+
 	fastify.get('/', async (request, reply) => {
 	  return { status: 'API is running' };
 	});
